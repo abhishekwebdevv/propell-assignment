4ae9d9e9cf2e0711a67d4a94e6e687fb4e10cb97
Abhishek Singh Dhakad <abhishekdhakad61@gmail.com>
2023-01-20 19:05:52 +0530

    update

--- src/Screens/Dashboard.js
+++ src/Screens/Dashboard.js
@@ -1,14 +1,25 @@
-import { Box, IconButton, Typography } from '@mui/material'
 import React from 'react'
+import { Box, IconButton, Typography } from '@mui/material'
+import { AiOutlineBell } from 'react-icons/ai'
 
 const Dashboard = () => {
-  return <Box>
-    <Box>
-      <Typography>Welcome Rahul</Typography>
-      <IconButton></IconButton>
+  return (
+    <Box padding={2}>
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Typography fontWeight="bold" fontSize={14}>
+          <span style={{ opacity: 0.6 }}>Welcome</span> Rahul
+        </Typography>
+        <IconButton>
+          <AiOutlineBell
+            size={22}
+            style={{
+              color: '#5516aa',
+            }}
+          />
+        </IconButton>
+      </Box>
     </Box>
-
-  </Box>
+  )
 }
 
 export default Dashboard
