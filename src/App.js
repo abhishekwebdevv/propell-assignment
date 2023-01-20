c13088767a0483bef40664b8dbe5299974afc205
Abhishek Singh Dhakad <abhishekdhakad61@gmail.com>
2023-01-20 18:24:11 +0530

    project initialized

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import { Box } from '@mui/material'
+
+import Dashboard from './Screens/Dashboard'
+
+function App() {
+  return (
+    <Box>
+      <Dashboard />
+    </Box>
+  )
+}
+
+export default App
---
+++ src/Screens/Dashboard.js
@@ -0,0 +1,14 @@
+import { Box, IconButton, Typography } from '@mui/material'
+import React from 'react'
+
+const Dashboard = () => {
+  return <Box>
+    <Box>
+      <Typography>Welcome Rahul</Typography>
+      <IconButton></IconButton>
+    </Box>
+
+  </Box>
+}
+
+export default Dashboard
